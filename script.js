6496963c8653089778491e2ac8579ec9e76415c2
Hhbhari <159613769+Hhbhari@users.noreply.github.com>
2024-02-24 20:21:54 +0530

    add logic to remove child nodes

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
+const grid = document.querySelector('.grid-container');
+const slide = document.querySelector('.slider');
+const span = document.querySelector('span');
+
+
 function makeGridLayout(){
-    const grid = document.querySelector('.grid-container');
-    const slide = document.querySelector('.slider');
-    const span = document.querySelector('span');
     slide.oninput = ()=>{
         const size = slide.value;
+        removeChildNode();
         span.innerHTML = size;
         console.log(size);
     }
 }
 
+function removeChildNode(){
+    while(grid.hasChildNodes()){
+        console.log()
+        grid.removeChild(grid.firstChild)
+    }
+}
 makeGridLayout();
