5ddb04a3028a237bc7196ea691d2d5a48069d5d5
Hhbhari <159613769+Hhbhari@users.noreply.github.com>
2024-02-24 20:48:13 +0530

    add logic to make child grids

--- script.js
+++ script.js
@@ -6,16 +6,29 @@ const span = document.querySelector('span');
 function makeGridLayout(){
     slide.oninput = ()=>{
         const size = slide.value;
+        const gridWidth = parseInt(getComputedStyle(grid).width);
         removeChildNode();
+        for(let i=1; i<=size*size;i++){
+            grid.appendChild(makeGrid(gridWidth/size));
+        }
         span.innerHTML = size;
         console.log(size);
     }
 }
 
+function makeGrid(divSize){
+    const divChild = document.createElement('div');
+    divChild.classList.add('grid-item');
+    divChild.style.width = divChild.style.height = `${divSize}px`;
+    return divChild;
+}
+
 function removeChildNode(){
     while(grid.hasChildNodes()){
         console.log()
-        grid.removeChild(grid.firstChild)
+        grid.removeChild(grid.firstChild);
     }
 }
+
+
 makeGridLayout();
